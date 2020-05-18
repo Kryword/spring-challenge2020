16c05b124d0c82f75b9d19e1ec8153e272c1f21e
Kryword <kryword@gmail.com>
2020-05-18 02:04:36 +0200

    Basic formatting

--- game/pac.go
+++ game/pac.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Pac struct {
-	Id         int
-	Pos        Position
-	PacType    PacType
-	Cooldown   int
-	SpeedTurns int
-	Mine       bool
-	Target     Position
-	EnemyType  PacType
+	Id          int
+	Pos         Position
+	PacType     PacType
+	Cooldown    int
+	SpeedTurns  int
+	Mine        bool
+	Target      Position
+	EnemyType   PacType
 	CurrentMove PacMove
 }
 
@@ -28,13 +28,16 @@ func (p *Pac) Init(id, x, y int, pacType PacType, cooldown, speedTurns int, mine
 	p.Mine = mine
 }
 
-func (p Pac) GetMove() string{
+func (p Pac) GetMove() string {
 	switch p.CurrentMove {
-	case Move: return fmt.Sprintf("MOVE %d %d %d [%d, %d]", p.Id, p.Target.X, p.Target.Y, p.Target.X, p.Target.Y)
-	case Speed: return fmt.Sprintf("SPEED %d", p.Id)
+	case Move:
+		return fmt.Sprintf("MOVE %d %d %d [%d, %d]", p.Id, p.Target.X, p.Target.Y, p.Target.X, p.Target.Y)
+	case Speed:
+		return fmt.Sprintf("SPEED %d", p.Id)
 	case Switch:
 		winType, _ := p.EnemyType.GetWinningLosingType()
 		return fmt.Sprintf("SWITCH %d %s", p.Id, winType.ToStr())
-	default: return ""
+	default:
+		return ""
 	}
 }
